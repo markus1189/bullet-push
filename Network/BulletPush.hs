b94896f01239ede8317aa0db9ac8d224216cf410
Markus Hauck <markus1189@gmail.com>
2015-01-09 14:39:16 +0100

    New function `pushTo` allowing to specify target of push

--- Network/BulletPush.hs
+++ Network/BulletPush.hs
@@ -1,18 +1,30 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Network.BulletPush (push, PushType(..), Token, mkToken, getToken) where
+module Network.BulletPush ( pushTo
+                          , push
+
+                          , PushType (..)
+                          , PushTarget (..)
+
+                          , Token
+                          , getToken
+                          , mkToken
+                          ) where
 
 import           Control.Exception (try)
 import           Control.Lens.Operators hiding ((.=))
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Aeson (toJSON, ToJSON, object, (.=))
+import           Data.Aeson.Types (Pair, Value(Object))
 import qualified Data.ByteString.Lazy as L
 import           Data.Text (Text)
 import           Data.Text.Encoding (encodeUtf8)
 import           Network.HTTP.Client (HttpException)
 import           Network.Wreq
+import qualified Data.HashMap.Strict as M
 
 newtype Token = Token { getToken :: Text }
+
+data PushTarget = Broadcast | Email Text deriving Show
 
 data PushType = Note { noteTitle :: Text
                      , noteBody :: Text
@@ -43,10 +55,21 @@ instance ToJSON PushType where
                                           , "items" .= toJSON items
                                           ]
 
-push :: Token -> PushType -> IO (Either HttpException (Response L.ByteString))
-push (Token t) = liftIO . tryHttpException . postWith opts ep . toJSON
+pushTo :: PushTarget -> Token -> PushType -> IO (Either HttpException (Response L.ByteString))
+pushTo tgt (Token t) = liftIO . tryHttpException . postWith opts ep . insertTarget tgt . toJSON
   where ep = "https://api.pushbullet.com/v2/pushes"
         opts = defaults & auth ?~ oauth2Bearer (encodeUtf8 t)
 
+push :: Token -> PushType -> IO (Either HttpException (Response L.ByteString))
+push = pushTo Broadcast
+
 tryHttpException :: IO a -> IO (Either HttpException a)
 tryHttpException = try
+
+insertIntoObject :: Pair -> Value -> Value
+insertIntoObject (k,v) (Object o) = Object $ M.insert k v o
+insertIntoObject _ v = v
+
+insertTarget :: PushTarget -> Value -> Value
+insertTarget Broadcast v = v
+insertTarget (Email addr) v = insertIntoObject ("email" .= addr) v
