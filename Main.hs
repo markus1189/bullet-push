a46726f00fd9f620d386b75245d4e5b82472f73b
Markus Hauck <markus1189@gmail.com>
2015-01-09 12:24:23 +0100

    Add arguments for specifying token and add verbosity

--- Main.hs
+++ Main.hs
@@ -3,54 +3,104 @@
 
 #if __GLASGOW_HASKELL__ < 710
 import           Control.Applicative ((<*>), (<$>), pure)
-#else
-import           Control.Applicative ((<$>))
+import           Data.Monoid ((<>))
 #endif
 
+import           Control.Exception (SomeException(..), handle)
 import           Control.Lens.Operators
-import qualified Data.ByteString.Char8 as BC
+import           Control.Monad (guard)
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.Trans.Maybe (runMaybeT, MaybeT(..))
 import           Data.List (dropWhileEnd)
-import           Data.Monoid ((<>))
+import           Data.Monoid (First(..))
+import           Data.Text (Text)
 import qualified Data.Text as T
-import           Network.Wreq
-import qualified Options.Applicative as O
+import qualified Data.Text.Encoding as T
+import           Network.Wreq hiding (header)
+import           Options.Applicative
+import           Prelude hiding (log)
 import           System.Directory (getHomeDirectory, doesFileExist)
 import           System.FilePath ((</>))
 
-import Network.BulletPush (push, PushType(..))
 
-tokenFile :: FilePath
-tokenFile = ".bulletpush"
+import           Network.BulletPush (push, PushType(..))
+
+defaultTokenFile :: FilePath
+defaultTokenFile = ".bulletpush"
+
+tokenFilePath :: FilePath -> IO FilePath
+tokenFilePath path = (</>) <$> getHomeDirectory <*> pure path
+
+data Verbosity = Normal | Verbose
+
+data CmdlineOpts = CmdlineOpts { givenVerbosity :: Verbosity
+                               , givenToken :: Maybe Text
+                               , tokenFile :: FilePath
+                               , pushType :: PushType
+                               }
+
+log :: Verbosity -> String -> IO ()
+log Normal _ = return ()
+log Verbose s = putStrLn s
 
 main :: IO ()
 main = do
-  tokenFilePath <- (</>) <$> getHomeDirectory <*> pure tokenFile
-  dotFileExists <- doesFileExist tokenFilePath
-  token <- if dotFileExists
-              then BC.pack . dropWhileEnd (`elem` ['\r','\n']) <$> readFile tokenFilePath
-              else error "'~/.bulletpush' not found, exiting."
-  eitherResponse <- O.execParser cmdlineOpts >>= push (opts token)
-  case eitherResponse of
-    Left _ -> putStrLn "Failed."
-    Right _ -> putStrLn "Success."
+  cmdOpts <- execParser cmdlineOpts
+  tokenFilePath' <- tokenFilePath (tokenFile cmdOpts)
+  pbTokenFile <- readTokenFile tokenFilePath'
+  let pbToken = getFirst $ foldMap First [givenToken cmdOpts, pbTokenFile]
+  case pbToken of
+    Nothing -> error "No token given and no ~/.bulletpush file."
+    Just token -> do
+      log (givenVerbosity cmdOpts) $ "Using token: " ++ (T.unpack token)
+      eitherResponse <- push (opts token) . pushType $ cmdOpts
+      case eitherResponse of
+        Left e -> log (givenVerbosity cmdOpts) (show e) >> putStrLn "Failed."
+        Right _ -> putStrLn "Success."
   where
-    cmdlineOpts = O.info (O.helper <*> cmds)
-                  ( O.fullDesc
-                    <> O.progDesc "Push something with pushbullet."
-                    <> O.header "Haskell pushbullet client" )
-    opts t = defaults & auth ?~ oauth2Bearer t
+    cmdlineOpts = info (helper <*> cmds)
+                  ( fullDesc
+                    <> progDesc "Push something with pushbullet."
+                    <> header "Haskell pushbullet client" )
+    opts t = defaults & auth ?~ oauth2Bearer (T.encodeUtf8 t)
 
-cmds :: O.Parser PushType
-cmds = O.subparser ( O.command "note" (O.info noteParser (O.progDesc "Push a note"))
-                    <> O.command "link" (O.info linkParser (O.progDesc "Push a link")))
+cmds :: Parser CmdlineOpts
+cmds = CmdlineOpts <$> verbosity
+                   <*> optional (T.pack <$> tokenOpt)
+                   <*> tokenFileOpt
+                   <*> subparser (command "note" (info noteParser (progDesc "Push a note"))
+                   <> command "link" (info linkParser (progDesc "Push a link")))
+  where tokenOpt =
+          strOption (long "token"
+                  <> metavar "TOKEN"
+                  <> help "Use TOKEN for authentication")
+        tokenFileOpt =
+          strOption (long "token-file"
+                  <> metavar "FILE"
+                  <> help ("Read authentication token from FILE, defaults to: ~/" <> defaultTokenFile)
+                  <> value defaultTokenFile)
+        verbosity = flag Normal Verbose (long "verbose" <> short 'v' <> help "Enable verbose mode")
 
-noteParser :: O.Parser PushType
+noteParser :: Parser PushType
 noteParser = Note
-         <$> (T.pack <$> O.argument O.str (O.metavar "TITLE"))
-         <*> (T.pack <$> O.argument O.str (O.metavar "BODY"))
+         <$> (T.pack <$> argument str (metavar "TITLE"))
+         <*> (T.pack <$> argument str (metavar "BODY"))
 
-linkParser :: O.Parser PushType
+linkParser :: Parser PushType
 linkParser = Link
-         <$> (T.pack <$> O.argument O.str (O.metavar "TITLE"))
-         <*> (T.pack <$> O.argument O.str (O.metavar "URL"))
-         <*> O.optional (T.pack <$> O.argument O.str (O.metavar "BODY"))
+         <$> (T.pack <$> argument str (metavar "TITLE"))
+         <*> (T.pack <$> argument str (metavar "URL"))
+         <*> optional (T.pack <$> argument str (metavar "BODY"))
+
+readTokenFile :: FilePath -> IO (Maybe Text)
+readTokenFile path = runMaybeT $ do
+  contents <- MaybeT $ maybeReadFile path
+  let ls = lines contents
+  guard . not . null $ ls
+  return . T.pack . dropWhileEnd (== '\r') . head $ ls
+
+maybeReadFile :: FilePath -> IO (Maybe String)
+maybeReadFile path = handle (\(SomeException _) -> pure Nothing) . runMaybeT $ do
+  exists <- liftIO $ doesFileExist path
+  guard exists
+  liftIO $ readFile path
