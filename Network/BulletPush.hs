5a72fce4a6df940764b10449b03982ff1b2ec63f
Markus Hauck <markus1189@gmail.com>
2015-01-09 13:38:40 +0100

    Switch from Wreq's `Postable` to JSON encoding

--- Network/BulletPush.hs
+++ Network/BulletPush.hs
@@ -3,16 +3,16 @@
 module Network.BulletPush (push, PushType(..), Token, mkToken, getToken) where
 
 import           Control.Exception (try)
-import           Control.Lens.Operators
+import           Control.Lens.Operators hiding ((.=))
 import           Control.Monad.IO.Class (liftIO)
+import           Data.Aeson (toJSON, ToJSON, object, (.=))
 import qualified Data.ByteString.Lazy as L
 import           Data.Text (Text)
 import           Data.Text.Encoding (encodeUtf8)
 import           Network.HTTP.Client (HttpException)
 import           Network.Wreq
-import           Network.Wreq.Types (Postable(..))
 
-newtype Token = Token {getToken :: Text}
+newtype Token = Token { getToken :: Text }
 
 data PushType = Note { noteTitle :: Text
                      , noteBody :: Text
@@ -27,20 +27,17 @@ data PushType = Note { noteTitle :: Text
 mkToken :: Text -> Maybe Token
 mkToken = Just . Token
 
-instance Postable PushType where
-  postPayload (Note title body) = postPayload [ "type" := ("note" :: Text)
-                                              , "title" := title
-                                              , "body" := body
-                                              ]
-
-  postPayload (Link title url maybeBody) = postPayload [ "type" := ("link" :: Text)
-                                                       , "title" := title
-                                                       , "body" := maybeBody
-                                                       , "url" := url
-                                                       ]
+instance ToJSON PushType where
+  toJSON (Note title body) = object [ "type" .= ("note" :: Text)
+                                    , "title" .= title
+                                    , "body" .= body]
+  toJSON (Link title url maybeBody) = object $ [ "type" .= ("link" :: Text)
+                                               , "title" .= title
+                                               , "url" .= url
+                                               ] ++ maybe [] (return . ("body" .=)) maybeBody
 
 push :: Token -> PushType -> IO (Either HttpException (Response L.ByteString))
-push (Token t) = liftIO . tryHttpException . postWith opts ep
+push (Token t) = liftIO . tryHttpException . postWith opts ep . toJSON
   where ep = "https://api.pushbullet.com/v2/pushes"
         opts = defaults & auth ?~ oauth2Bearer (encodeUtf8 t)
 
